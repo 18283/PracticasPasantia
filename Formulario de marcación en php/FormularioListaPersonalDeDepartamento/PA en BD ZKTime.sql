04c2af8813a1deb0107013ddb4b5138ca68e339e
Eunice <63760394+18283@users.noreply.github.com>
2025-04-17 17:10:37 -0400

    Add files via upload

---
+++ Formulario de marcación en php/FormularioListaPersonalDeDepartamento/PA en BD ZKTime.sql
@@ -0,0 +1,49 @@
+Create procedure PA_RegistrosDeMarcacion (@CI varchar(24))
+as
+begin
+	select U.USERID, U.NAME, M.MachineAlias, C.CHECKTIME, C.CHECKTYPE 
+	from CHECKINOUT as C,Machines as M,USERINFO as U 
+	where (C.sn = M.sn and U.USERID = C.USERID) and (U.BADGENUMBER = @CI);
+end
+
+Create procedure PA_RegistrosDeMarcacionEntreFechas(@CI varchar(24), @fechaInicio datetime, @fechaFin datetime)
+as
+begin
+	select U.USERID, U.NAME, M.MachineAlias, C.CHECKTIME, C.CHECKTYPE 
+	from CHECKINOUT as C,Machines as M,USERINFO as U 
+	where C.sn = M.sn and U.USERID = C.USERID and U.BADGENUMBER = @CI and C.CHECKTIME >= @fechaInicio and C.CHECKTIME <= @fechaFin;
+end
+
+--QUE devuelva una lista del personal, que reciba cod dpto
+Create procedure PA_PersonalDeDPTO(@IDEPT smallint)
+as
+begin
+	select U.USERID, U.NAME
+	from USERINFO as U 
+	where DEFAULTDEPTID=@IDEPT;
+end
+
+--CI=BADGENUMBER, id=USERID, nombre=NAME, fecha=CHECKTIME, marcacion=CHECKTYPE,  marcador= MachineAlias
+-----------------------------------------------------------------------------------------------------------------------
+
+--drop procedure if exists PA_RegistrosDeMarcacion;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+execute PA_RegistrosDeMarcacion '8943725';
+
+execute PA_RegistrosDeMarcacionEntreFechas '8943725', '2025-04-01', '2025-04-16';
+
+EXECUTE PA_PersonalDeDPTO 3;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+select U.BADGENUMBER AS CI,U.NAME AS NOMBRE,M.MachineAlias MARCADOR,C.CHECKTIME FECHA,C.CHECKTYPE 'ING/SALIDA' 
+from CHECKINOUT as C,Machines as M,USERINFO as U 
+where (C.sn = M.sn and U.USERID = C.USERID) and (U.BADGENUMBER = '8217852');
+
+
+
+select U.USERID, U.NAME, M.MachineAlias, C.CHECKTIME, C.CHECKTYPE 
+from CHECKINOUT as C,Machines as M,USERINFO as U 
+where (C.sn = M.sn and U.USERID = C.USERID) and (U.BADGENUMBER = '8943725');
